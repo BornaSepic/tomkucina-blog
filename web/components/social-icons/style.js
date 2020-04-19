699599c38ffcaaec2881a9305eaa7960efe3aea4
Max1mmus <56004853+Max1mmus@users.noreply.github.com>
2020-04-19 13:52:45 +0200

    added header style

--- web/components/social-icons/style.js
+++ web/components/social-icons/style.js
@@ -1,11 +1,19 @@
 import styled from 'styled-components';
 
 export const SocialIcon = styled.a`
-    margin: 0 8px;
-    color: #3F729B;
+    margin : 0 8px;
+    color : ${props => props.header ? "black" : "white"}
 `;
 
 export const SocialWrapper = styled.div`
-    text-align: center;
-    margin: 0;
+${props => props.header ? {
+    "text-align" : "right;",
+    "margin": "0px;",
+    "border-bottom" : "solid 1px #9c9a9a",
+
+} : {
+    "text-align" : "center;",
+    "border-bottom": "none;",
+    "padding-bottom" : "30px;"
+}}
 `;
