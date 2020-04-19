260ea2a4a6f3dc5fae3aa4e659f8bc2c4db09cb7
Max1mmus <56004853+Max1mmus@users.noreply.github.com>
2020-04-19 13:53:19 +0200

    remove pageTitle added footer style

--- web/components/footer/style.js
+++ web/components/footer/style.js
@@ -1,13 +1,6 @@
 import styled from 'styled-components';
 
-export const PageTitle = styled.a`
-    cursor: pointer;
-    color: #333;
-    text-decoration: none;
-    display: block;
-    margin-top: 5px;
-    font-size: 2.5rem;
-    text-align: center;
-    font-family: 'Playfair Display', serif;
-    font-weight: 900;
+export const Footer = styled.footer`
+    background-color: #4c4c4c;
+    margin-bottom: 10px;
 `;
