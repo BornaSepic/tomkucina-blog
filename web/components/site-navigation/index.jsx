8f6d8bb05451536e74c4db18c5940a69903c8f75
Max1mmus <56004853+Max1mmus@users.noreply.github.com>
2020-04-19 13:54:02 +0200

    initial commit

---
+++ web/components/site-navigation/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Link from "next/link";
+import * as Styled from "./style";
+
+const navItems = [
+    {
+        ref : '/home',
+        content: 'Home'
+    },
+    {
+        ref: '/blog',
+        content: 'Blog'
+    },
+    {
+        ref: '/about',
+        content: 'About'
+    },
+    {
+        ref: '/contact',
+        content: 'Contact'
+    }
+];
+
+const SiteNavigation = (props) => {
+    return (
+        <Styled.NavbarWrapper header={props.header}>
+                {
+                    navItems.map(item => (
+                        <Link href={item.ref}>
+                            <Styled.NavLinks>{item.content.toUpperCase()}</Styled.NavLinks>
+                        </Link>
+                    ))
+                }
+        </Styled.NavbarWrapper>
+    );
+};
+
+export default SiteNavigation;
