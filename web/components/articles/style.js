b3f458f124b4c34d4dc6e22141450c69066d1221
Max1mmus <56004853+Max1mmus@users.noreply.github.com>
2020-04-24 16:39:21 +0200

    initial commit

---
+++ web/components/articles/style.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const OuterContainer = styled.div`
+    font-size: 0px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`
+
+export const ImageContainer = styled.div`
+
+`
+
+export const ContinueReading = styled.a`
+    color: white;
+    background: black;
+    padding: 4px;
+    width: max-content;
+    font-size: 17px;
+    cursor: pointer;
+    margin: 10px auto;
+`
+export const ArticleContent = styled.div`
+    width: 48%;
+    margin: 0px;
+`
+
+export const Image = styled.img`
+    width: 100%;
+`
+
+export const ArticleContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+export const ArticleTitle = styled.h1`
+    font-family: 'Elephant',serif;
+    font-size: 25px;
+`
+export const ArticleParagraph = styled.p`
+    
+`
