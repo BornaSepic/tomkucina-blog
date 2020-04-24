bbf3f3e6f6465768134e7d63a6acc8876a89bb30
Max1mmus <56004853+Max1mmus@users.noreply.github.com>
2020-04-24 02:23:57 +0200

    initial commit

---
+++ web/components/all-posts-link/style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const PostsLink = styled.a`
+    color: white;
+    background: black;
+    padding: 4px;
+    width: max-content;
+    font-size: 20px;
+    text-decoration: none;
+    cursor: pointer;
+`
+export const Container = styled.div`
+    padding: 25px;
+    background-image: url("https://ucc2057cb2a24d97c3ad2915379f.previews.dropboxusercontent.com/p/thumb/AAwxkfINaCGDNmpJuH39AV-x6CYHkZrrfkX6Ql2Cp7gbnSh5Bk1NQG6RtDfgsrfmgjjiYLm_OQsooveD2a1yXXPg3J95omBMwkgIY4lzZTtkcq02Ri9z0ym6hT4gGE9BgdUDWFrR4eP-VjgiCneyg8NN1qkAvLcTMf_L0JLlUpMhyN3RtGogdAgwbWgYRYjNlytrooPQ3QtXcRft_hHHTvQnwfYcTArbTwKoYrSv5sVFVlj4_4ciqjC61Xf8Nto5BnR9qxJk2zZl9MSXMRmCc3laHzzj9nCS9tzRqyxzU_9PuGekXRF4rPg3JbstLoH4njBdDNTNyjYFNlSU6oToIXix3PU5eI8VtINVFmQHwk89wA/p.png?fv_content=true&size_mode=5");
+    background-size: cover;
+    text-align: center;
+`
