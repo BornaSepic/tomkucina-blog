0b86790f52b2ebf2f890b6a92e73627ccefcbc55
Max1mmus <56004853+Max1mmus@users.noreply.github.com>
2020-04-24 02:23:40 +0200

    initial commit

---
+++ web/components/all-posts-link/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as Styled from "./style";
+
+const AllPostsLink = () => {
+    return (
+        <Styled.Container>
+            <Styled.PostsLink href="/">SVE OBJAVE PRONAĐI OVDJE</Styled.PostsLink>
+        </Styled.Container>
+    )
+}
+
+export default AllPostsLink;
